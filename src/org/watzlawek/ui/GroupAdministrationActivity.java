d7672ada7f7e030b5c3d0695da9cd9d24256b563
Safran Quader <safran.quader@googlemail.com>
2015-01-10 15:10:52 +0100

    Update GroupAdministrationActivity

--- src/org/watzlawek/ui/GroupAdministrationActivity.java
+++ src/org/watzlawek/ui/GroupAdministrationActivity.java
@@ -4,6 +4,8 @@ import org.watzlawek.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Activity to display the groupadministration.
@@ -18,6 +20,12 @@ public class GroupAdministrationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groupadministration);
+        
+        String[] user = new String[]{"Benutzer 1", "Benutzer 2"};
+        
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, user);
+        ListView listView = (ListView) findViewById(R.id.GroupadministrationContactlist);
+        listView.setAdapter(arrayAdapter);
 	}
 	
 }
