33eff65510d31b85c61ed493b16e251d4864a815
Stefan Wegerhoff <stefan.wegerhoff@auster-gmbh.de>
2015-01-11 15:23:15 +0100

    Konstruktoren und decrypt encrypt ergaenzt, sowie eine Exception

--- src/encryption/org/watzlawek/Encryption.java
+++ src/encryption/org/watzlawek/Encryption.java
@@ -1,5 +1,26 @@
 package encryption.org.watzlawek;
 
-public class Encryption {
+import java.util.Vector;
+import java.lang.String;
 
+public class Encryption {
+	private Vector<String> jid;
+	private String context;
+	
+	public Encryption (String context){
+		
+	}
+	
+	public Encryption (String bohoo, String context){
+		
+	}
+
+	public void decrypt(String cipher) throws EncryptionFaultException {
+		
+	}
+	
+	public void encrypt(String text) throws EncryptionFaultException {
+		
+	}
+	
 }
---
+++ src/encryption/org/watzlawek/EncryptionFaultException.java
@@ -0,0 +1,24 @@
+package encryption.org.watzlawek;
+
+public class EncryptionFaultException extends Exception {
+
+	public EncryptionFaultException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EncryptionFaultException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EncryptionFaultException(Throwable throwable) {
+		super(throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EncryptionFaultException(String detailMessage, Throwable throwable) {
+		super(detailMessage, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
