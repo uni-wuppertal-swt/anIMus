61edaca8d796828092aa1eba6c77c9851c96cdd2
toeffes <toeffes_schlueter@yahoo.de>
2015-01-14 18:22:11 +0100

    - Generated an internal class Contact for handling contacts between methods more easily. - began writing the compareTo(Vector<IMChat> contacts, int serverID) method. - created method private Vector<Contact> getDBContacts, which will provide all 	entries in the DB as Contact-elements as a Vector - Changed return-value of getJIDsUsernames(int serverID) to Vector<String> where 	one entry will be the JID and the next the corresponding username, and so on. 	Now this method will need the serverID given by the calling part. - Sorted methods alphabetically - added a private final static String DB_COLUMN_SERVERID to save the serverID in 	the DB properly.

--- src/org/watzlawek/contactmanager/ContactDatabaseHandler.java
+++ src/org/watzlawek/contactmanager/ContactDatabaseHandler.java
@@ -14,10 +14,27 @@ public class ContactDatabaseHandler extends SQLiteOpenHelper{
 	private static final int DATABASE_VERSION = 1;
 	private static final String DB_TABLE_NAME = "contacts";
 	private static final String DB_COLUMN_JID = "jid";
-	private static final String DB_COLUMN_INVISIBLE = "invisible";
-	private static final String DB_COLUMN_NOTE = "note";
 	private static final String DB_COLUMN_USERNAME = "username";
+	private static final String DB_COLUMN_NOTE = "note";
+	private static final String DB_COLUMN_SERVERID = "serverid";
+	private static final String DB_COLUMN_INVISIBLE = "invisible";
+	
 
+	private class Contact{
+		public String jid;
+		public String username;
+		public String note;
+		public int serverID;
+		public boolean visible;
+		
+		public Contact(String inJID, String inUsername, String inNote, int inSID, boolean inVisible) {
+			jid = inJID;
+			username = inUsername;
+			note = inNote;
+			serverID = inSID;
+			visible = inVisible;
+		}
+	}
 
 	public ContactDatabaseHandler(android.content.Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -26,9 +43,10 @@ public class ContactDatabaseHandler extends SQLiteOpenHelper{
 	public void onCreate(android.database.sqlite.SQLiteDatabase db) {
 		String createDB = "CREATE TABLE " +DB_TABLE_NAME +"(" +
 				DB_COLUMN_JID +" VARCHAR(30) PRIMARY KEY,"+
-				DB_COLUMN_INVISIBLE +" INTEGER,"+
+				DB_COLUMN_USERNAME +" VARCHAR(30)" +
 				DB_COLUMN_NOTE +" VARCHAR(140),"+
-				DB_COLUMN_USERNAME +" VARCHAR(30))";
+				DB_COLUMN_SERVERID + " INTEGER"+
+				DB_COLUMN_INVISIBLE +" INTEGER)";
 		db.execSQL(createDB);
 	}
 
@@ -58,20 +76,53 @@ public class ContactDatabaseHandler extends SQLiteOpenHelper{
 
 	
 	
-	private Vector<String> read_jids() {
-		// TODO - implement ContactDatabaseHandler.read_jids
-		throw new UnsupportedOperationException();
+	/**
+	 * This method compares the contacts on the server(serverID) with the 
+	 * corresponding contacts in the DB. It pulls the contacts of the DB
+	 * and compares then.
+	 * @param contacts
+	 */
+	public void compareContacts(Vector<IMChat> contacts, int serverID) {
+		/*Vector<String> jidList = read_jids();
+		Vector<Integer> sID = getServerIDs(); */
+		Vector<Contact> dbContacts = getDBContacts();
+		
+		// Create a Vector<Contact> where only contacts are saved,
+		// which are visible and available on the currently connected server
+		Vector<Contact> neededContacts = new Vector<Contact>();
+		for (Contact c : dbContacts){
+			if (c.serverID == serverID && c.visible == true) 
+				neededContacts.add(c);
+		}
+		
+		// Create a new Vector<Contact>, which extracts all important 
+		// information of IMChat and stores it. 
+		Vector<Contact> serverContacts = new Vector<Contact>();
+		for (IMChat c : contacts) {
+			Contact contact = new Contact(((XMPPChat)c).get_jid(), c.get_username(), c.get_note(), serverID, c.isVisible());
+		}
+		// Now the comparison can start: 
+		
 	}
 
 	
-	
-	/**
-	 * 
-	 * @param contacts
-	 */
-	public void compareContacts(Vector<IMChat> contacts) {
-		// is changed in a moment...
+	private Vector<Contact> getDBContacts() {
+		// This is the real reading-method with DB access.
+		// ...
+		return new Vector<Contact>(); // Dummy
 	}
+	
+	
+	public Vector<String> getJIDsUsernames(int serverID) {
+		Vector<Contact> dbContacts = getDBContacts();
+		/* 
+		 * Here must be filtered: Is the contact in the ServerID, which is 
+		 * requesting this List and is the contact visible?
+		 */
+		return new Vector<String>(); // Dummy
+	}
+	
+	
 
 	
 	
@@ -87,6 +138,11 @@ public class ContactDatabaseHandler extends SQLiteOpenHelper{
 	}
 
 	
+	private Vector<String> read_jids() {
+		// TODO - implement ContactDatabaseHandler.read_jids
+		throw new UnsupportedOperationException();
+	}
+	
 	
 	/**
 	 * 
@@ -97,13 +153,6 @@ public class ContactDatabaseHandler extends SQLiteOpenHelper{
 	 */
 	private void updateContact(String in_jid, String in_username, String in_note, int in_invisible) {
 		// TODO - implement ContactDatabaseHandler.updateContact
-		throw new UnsupportedOperationException();
-	}
-
-	
-	
-	public HashMap<String, String> getJIDsUsernames() {
-		// TODO - implement ContactDatabaseHandler.getJIDsUsernames
 		throw new UnsupportedOperationException();
 	}
 
