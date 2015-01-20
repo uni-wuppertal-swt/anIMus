92c50d5664750ec7683eb50e2f6cd7cec0deb652
fbettray <fredbett@live.de>
2015-01-20 21:02:36 +0100

    AESCipher hinzugefügt und in Verbindung gebracht mit Ver- und Entschlüsselungsfunktion.

--- src/encryption/org/watzlawek/Encryption.java
+++ src/encryption/org/watzlawek/Encryption.java
@@ -1,22 +1,10 @@
 package encryption.org.watzlawek;
 
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Vector;
-
-import java.lang.StringBuffer;
-import java.lang.String;
-import android.content.*;
-import org.jivesoftware.smack.packet.*;
-
-import android.widget.Toast;
-
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 //import org.jivesoftware.smack.packet.Presence;
 
 public class Encryption {
@@ -164,109 +152,22 @@ public class Encryption {
 	}*/
 	
 	public Message decryptMessage( Message cipher) throws EncryptionFaultException {
+		Message plain;
+		MessageKey MK;
+		plain = decrypt("",MK,cipher);
+		
+		return plain;
+
+		
+	}
+
+	public Message encryptMessage(Message plain) throws EncryptionFaultException {
+		Message cipher;
+		MessageKey MK;
+		cipher = encrypt("",MK,plain);
+
 		return cipher;
-
-		
 	}
-		/**
-		 * MessageKey MK,
-		 * 
-		 * 
-		 * Muessen noch deklariert werden:
-
-		message_key MK;
-		Np  : Purported message number
-		PNp : Purported previous message number
-		CKp : Purported new chain key
-		DHp : Purported new DHr
-		RKp : Purported new root key
-		NHKp, HKp : Purported new header keys*/
-		
-		/**
-		 * Attempt to decrypt the message
-		  with skipped-over message keys (and their associated header keys) from
-		  persistent storage.
-		  */
-		//try_skipped_header_and_message_keys();
-		
-		/**
-		 * Given a current header key,
-		  a current message number, a future message number, and a chain key,
-		  calculates and stores all skipped-over message keys (if any) in a
-		  staging area where they can later be committed, along with their
-		  associated header key.  Returns the chain key and message key
-		  corresponding to the future message number.  If passed a chain key
-		  with value <none>, this function does nothing.
-		*/
-		//stage_skipped_header_and_message_keys();
-		
-		/**
-		 * Commits any skipped-over
-		  message keys from the staging area to persistent storage (along 
-		  with their associated header keys).
-		*/
-		/*commit_skipped_header_and_message_keys();
-		
-		if (plaintext = try_skipped_header_and_message_keys())
-			return plaintext;
-		
-		if (HKr != 0 && Dec(HKr, header)){
-			Np = read();
-			MK = stage_skipped_header_and_message_keys(HKr, Nr, Np, CKr);
-			CKp = MK;
-			if (!Dec(MK, ciphertext))
-				raise undecryptable;
-		}
-		else{
-			if (ratchet_flag || !Dec(NHKr, header))
-				raise undecryptable();
-			Np = read();
-			PNp = read();
-			DHRp = read();
-			stage_skipped_header_and_message_keys(HKr, Nr, PNp, CKr);
-			HKp = NHKr;
-			CKp = KDF( HMAC-HASH(RK, DH(DHRp, DHRs)) );
-			RKp = CKp;
-			NHKp = CKp; 
-			MK = stage_skipped_header_and_message_keys(HKp, 0, Np, CKp);
-			CKp = MK;
-			if (!Dec(MK, ciphertext))
-				raise undecryptable();
-			RK = RKp;
-			HKr = HKp;
-			NHKr = NHKp;
-			DHRr = DHRp;
-			erase(DHRs);
-			ratchet_flag = true;
-			commit_skipped_header_and_message_keys();
-			Nr = Np + 1;
-			CKr = CKp;
-			return read();
-		}
-	}*/
-	/*
-	public Message encryptMessage(MessageKey MK, Message text) throws EncryptionFaultException {
-		message_key MK;
-
-		if (ratchet_flag) {
-			DHRs = generateECDH();
-			HKs = NHKs;
-			CKs = KDF( HMAC-HASH(RK, DH(DHRs, DHRr)) );
-			RK = CKs;
-			NHKs = CKs;
-			PNs = Ns;
-			Ns = 0;
-			ratchet_flag = false;
-		}
-
-		MK = HMAC-HASH(CKs, "0");
-		text = Enc(HKs, Ns || PNs || DHRs) || Enc(MK, plaintext);
-		Ns = Ns + 1;
-		CKs = HMAC-HASH(CKs, "1");
-
-		return text;
-	}
-	*/
 
 	
 	
