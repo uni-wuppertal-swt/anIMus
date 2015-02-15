760d8d191475b23364a1274d4517e1cdd7e30039
Stefan Wegerhoff <stefan.wegerhoff@auster-gmbh.de>
2015-02-15 16:08:51 +0100

    Implementiert Methoden in den Header Zwischenstand

--- src/encryption/org/watzlawek/Header.java
+++ src/encryption/org/watzlawek/Header.java
@@ -1,9 +1,24 @@
 package encryption.org.watzlawek;
 
+import org.jivesoftware.smack.packet.Message;
+
 public class Header {
 
-	public Header() {
-		// TODO Auto-generated constructor stub
+	private byte[] hash;
+	private Key key;
+	private short manyOfKeysLeft;
+	private short manyOfKeysToSend;
+	private String coreID;
+	private String expectNextCore; 
+	
+	
+	
+	public Header(KeySetDB keyset, Message msg) {
+		
 	}
 
+	public Header(KeySetDB keyset){
+		
+	}
+	
 }
